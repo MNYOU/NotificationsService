1b02b7a85b2098f7195aae6bc777e97f4cbd85a4
Ilfir Sakhabutdinov <ilfir.sakhabutdinov.study@mail.ru>
2024-12-26 17:18:07 +0500

    Add. Добавил корневой проект

---
+++ src/EmailSender/EmailSender.Core/Common/Error.cs
@@ -0,0 +1,72 @@
+﻿namespace EmailSender.Core.Common;
+
+public class Error(StatusCode statusCode, string message)
+{
+    public StatusCode StatusCode { get; } = statusCode;
+    public string Message { get; } = message;
+
+    public static Error Internal(string message)
+    {
+        return new Error(StatusCode.InternalServiceError, message);
+    }
+
+    public static Error BadRequest(string message)
+    {
+        return new Error(StatusCode.BadRequest, message);
+    }
+
+    public static Error NotFound(string message)
+    {
+        return new Error(StatusCode.NotFound, message);
+    }
+
+    public static Error Conflict(string message)
+    {
+        return new Error(StatusCode.Conflict, message);
+    }
+
+    public static Error Unauthorized(string message)
+    {
+        return new Error(StatusCode.Unauthorized, message);
+    }
+
+    public static Error Forbidden(string message)
+    {
+        return new Error(StatusCode.Forbidden, message);
+    }
+
+    public static Error PreconditionFailed(string message)
+    {
+        return new Error(StatusCode.PreconditionFailed, message);
+    }
+
+    public static Error UnprocessableEntity(string message)
+    {
+        return new Error(StatusCode.UnprocessableEntity, message);
+    }
+
+    public static Error TooManyRequests(string message)
+    {
+        return new Error(StatusCode.TooManyRequests, message);
+    }
+
+    public static Error BadGateway(string message)
+    {
+        return new Error(StatusCode.BadGateway, message);
+    }
+
+    public static Error ServiceUnavailable(string message)
+    {
+        return new Error(StatusCode.ServiceUnavailable, message);
+    }
+
+    public static Error GatewayTimeout(string message)
+    {
+        return new Error(StatusCode.GatewayTimeout, message);
+    }
+
+    public override string ToString()
+    {
+        return $"Error: StatusCode = {(int) StatusCode}, Message = '{Message}'";
+    }
+}
---
+++ src/EmailSender/EmailSender.Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace EmailSender.Core.Extensions;
+
+public static class ObjectExtensions
+{
+    public static void EnsureNotNull<T>(this T? value) where T : class
+    {
+        ArgumentNullException.ThrowIfNull(value);
+    }
+}
