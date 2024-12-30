8a6b5a67e489e7909b1e8a510bac35e117211c67
Denis-GM <104080323+Denis-GM@users.noreply.github.com>
2024-12-30 13:04:08 +0400

    fix settings

--- src/SMSSender/SMSSender.Domain/Settings/SMSSettings.cs
+++ src/SMSSender/SMSSender.Domain/Settings/SMSSettings.cs
@@ -3,8 +3,8 @@
 public class SMSSettings
 {
     public string Project { get; set; } = "SMSSender";
-    public string ApiKey { get; set; } = "c5fjhg-f0F0h2K2f8W8w9N1b3H7k7!706";
-    public string Login { get; set; } = "denis-ft";
-    public string Password { get; set; } = "M7N-h6j-pZR-7R5";
+    public string ApiKey { get; set; } = "";
+    public string Login { get; set; } = "";
+    public string Password { get; set; } = "";
     public string Sender { get; set; } = "SMSC.RU";
 }
