fc6ff430be005d4e4e0a7d1b5f7918ac7e21e273
Ilfir Sakhabutdinov <ilfir.sakhabutdinov.study@mail.ru>
2025-01-01 16:26:52 +0500

    Add. Контроллеры в MessagePublisher

---
+++ src/MessagePublisher/MessagePublisher.Api/Controllers/SendController.cs
@@ -0,0 +1,21 @@
+﻿using MessagePublisher.Api.Controllers.Base;
+using MessagePublisher.Api.Extensions;
+using MessagePublisher.Core.Common;
+using MessagePublisher.Logic.Interfaces.Managers;
+using MessagePublisher.Logic.Models.Requests.Send;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessagePublisher.Api.Controllers;
+
+public class SendController(ISendManager sendManager) : ApiControllerBase
+{
+    [HttpPost(Routes.Routes.SendMessage)]
+    [ProducesDefaultResponseType(typeof(OperationResult))]
+    [ProducesResponseType<OperationResult>(StatusCodes.Status200OK)]
+    [ProducesResponseType<OperationResult>(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> SendMessage(SendMessageRequest sendMessage)
+    {
+        var sendResult = await sendManager.Send(sendMessage);
+        return sendResult.ToActionResult();
+    }
+}
