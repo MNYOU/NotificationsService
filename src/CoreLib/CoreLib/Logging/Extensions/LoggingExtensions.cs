9fcd7460c1e35cbb6cb119b3ac894c81d3ee2770
Ilfir Sakhabutdinov <ilfir.sakhabutdinov.study@mail.ru>
2025-01-02 23:39:08 +0500

    Add. Extensions для регистрации логеров

---
+++ src/CoreLib/CoreLib/Logging/Extensions/LoggingExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace CoreLib.Logging.Extensions;
+
+public static class LoggingExtensions
+{
+    public static IHostBuilder UseLogging(this IHostBuilder hostBuilder)
+    {
+        return hostBuilder.UseSerilog((context, services, configuration) =>
+        {
+            configuration
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day);
+        });
+    }
+    
+    public static IServiceCollection AddLogging(this IServiceCollection services, IConfiguration configuration)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+        
+        services.AddSingleton(Log.Logger);
+        return services;
+    }
+}
