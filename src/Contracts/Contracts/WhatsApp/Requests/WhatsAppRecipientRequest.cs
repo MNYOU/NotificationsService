d0b02d8996f2276b046825f75ee78ef63f2f9a6f
Ilfir Sakhabutdinov <ilfir.sakhabutdinov.study@mail.ru>
2025-01-03 17:33:34 +0500

    Fix. Исправил неточность в контракте whatsapp

--- src/Contracts/Contracts/WhatsApp/Requests/WhatsAppRecipientRequest.cs
+++ src/Contracts/Contracts/WhatsApp/Requests/WhatsAppRecipientRequest.cs
@@ -5,7 +5,7 @@ namespace Contracts.WhatsApp.Requests;
 public class WhatsAppRecipientRequest
 {
     [Required(ErrorMessage = "Номер телефона обязателен.")]
-    [RegularExpression(@"^(\+7|7)\d{10}$", 
+    [RegularExpression(@"^(\+7|7)(8)\d{10}$", 
         ErrorMessage = "Неверный формат номера телефона. Должен начинаться с +7 или 7, а затем должен идти номер телефона с 8 и содержать 10 цифр после этого.")]
     public required string PhoneNumber { get; set; }
 }
