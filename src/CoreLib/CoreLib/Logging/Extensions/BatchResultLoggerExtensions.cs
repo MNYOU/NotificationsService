a9d1cd3beaa1b1a6de586246d441751d265f6a0b
Ilfir Sakhabutdinov <ilfir.sakhabutdinov.study@mail.ru>
2025-01-03 14:21:03 +0500

    Add. Добавил методы расширения дл логирования BatchResult

---
+++ src/CoreLib/CoreLib/Logging/Extensions/BatchResultLoggerExtensions.cs
@@ -0,0 +1,41 @@
+﻿using CoreLib.Common;
+using Microsoft.Extensions.Logging;
+
+namespace CoreLib.Logging.Extensions;
+
+public static class BatchResultLoggerExtensions
+{
+    public static void LogBatchResult(this ILogger logger, BatchOperationResult result)
+    {
+        if (result.IsSuccess)
+        {
+            logger.LogInformation("Batch operation: {message}", result.ToString());
+        }
+        else
+        {
+            logger.LogError("Batch operation: {message}", result.ToString());
+        }
+    }
+
+    public static void LogBatchResult<TResult>(this ILogger logger, BatchOperationResult<TResult> result)
+    {
+        if (result.IsSuccess)
+        {
+            logger.LogInformation("Batch operation: {message}", result.ToString());
+        }
+        else
+        {
+            logger.LogError("Batch operation: {message}", result.ToString());
+        }
+    }
+
+    public static void LogBatchResult<T>(this ILogger<T> logger, BatchOperationResult result)
+    {
+        LogBatchResult((ILogger)logger, result);
+    }
+
+    public static void LogBatchResult<T, TResult>(this ILogger<T> logger, BatchOperationResult<TResult> result)
+    {
+        LogBatchResult((ILogger)logger, result);
+    }
+}
