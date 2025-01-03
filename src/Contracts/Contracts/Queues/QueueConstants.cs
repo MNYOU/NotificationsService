6227184f69a79f564a7bb90ac27913c51f93b180
Ilfir Sakhabutdinov <ilfir.sakhabutdinov.study@mail.ru>
2025-01-03 17:33:53 +0500

    Add. Добавил константы очередей

---
+++ src/Contracts/Contracts/Queues/QueueConstants.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+
+namespace Contracts.Queues;
+
+[PublicAPI]
+public static class QueueConstants
+{
+    public static string WhatsappQueueName => "whatsapp";
+    public static string SmsQueueName => "sms";
+    public static string EmailQueueName => "email";
+}
